fbe6da13ba8c1cb1f43c2f8f605bf05d486a285f
BankKeo <77560510+BankKeo@users.noreply.github.com>
2022-06-02 09:59:40 +0700

    first commit

---
+++ flutter_application_4/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late SharedPreferences prefs;
+  List<String> entries = [];
+  List<String> data = [];
+
+  final textController = TextEditingController();
+
+  void _saveData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setStringList('historyValue', entries);
+    data = prefs.getStringList('historyValue')!;
+    setState(() {});
+  }
+
+  @override
+  void initState() async {
+    super.initState();
+    _saveData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    entries.sort();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: TextField(
+            controller: textController,
+          ),
+        ),
+        body: data.length > 0
+            ? ListView.separated(
+                itemCount: data.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('${data[index]}'),
+                    onLongPress: () {
+                      setState(() {
+                        data.removeAt(index);
+                      });
+                    },
+                    trailing: Icon(Icons.remove_circle_outline),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(
+                  color: Colors.black,
+                ),
+              )
+            : Center(
+                child: Text("No Groceries"),
+              ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.save),
+          onPressed: () {
+            entries.add(textController.text);
+            textController.clear();
+            _saveData();
+          },
+        ),
+      ),
+    );
+  }
+}
